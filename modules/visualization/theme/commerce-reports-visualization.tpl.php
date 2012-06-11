0efb57224ed7675c4bfb836e70e6159abd97bac3
Christophe Van Gysel <chris@stophr.be>
2012-06-11 03:01:17 +0200

    Initial commit for commerce_reports_visualization

---
+++ modules/visualization/theme/commerce-reports-visualization.tpl.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * @file
+ * Template file returns HTML for Commerce Reports Visualization.
+ *
+ * @param $chart
+ *
+ * @return
+ *   The chart div, ready for the highchart JS to render to.
+ *
+ * @see theme_highcharts_chart()
+ *
+ * @todo is this file necessary since we have a theme function?
+ */
+ 
+  highcharts_render($chart);
+?>
+<div id="<?php print $chart->chart->renderTo; ?>" class="highcharts-chart" />
