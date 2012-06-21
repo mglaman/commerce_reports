afcbede4224be9d9b9e8c6282ddee41a1681d604
Christophe Van Gysel <chris@stophr.be>
2012-06-21 22:14:19 +0200

    Added basic (buggy) google visualization support

--- modules/visualization/js/visualization.js
+++ modules/visualization/js/visualization.js
@@ -7,16 +7,49 @@ Drupal.behaviors.commerce_reports = {
   charts: [],
 	attach: function(context) {
     $.each($(".commerce_reports-chart", context).not(".commerce_reports-processed"), function(idx, value) {
-      chart_id = $(value).attr("id");
-      var settings = Drupal.settings.commerce_reports[chart_id];
-      if (settings !== undefined) {
-        
-        var settings = Drupal.settings.commerce_reports[chart_id];
-        if (settings !== undefined) {
-          // Create the chart.
-          Drupal.commerce_reports.charts[chart_id] = new Highcharts.Chart(settings);
-          $(value).addClass("highcharts-processed");
+      var chart_id = $(value).attr("id");
+      var chart = Drupal.settings.commerce_reports[chart_id];
+      
+      if (chart !== undefined) {
+        switch (chart['library']) {
+          case 'highcharts':
+            Drupal.commerce_reports.charts[chart_id] = new Highcharts.Chart(chart.options);
+            break;
+            
+          case 'google_visualization':
+            console.log(chart);
+            function drawChart() {
+              var data = google.visualization.arrayToDataTable(chart.dataArray);
+              var chartElement = document.getElementById(chart.chart_id);
+              
+              switch (chart.type) {
+                case 'line':
+                  Drupal.commerce_reports.charts[chart_id] = new google.visualization.LineChart(chartElement);
+                  break;
+                  
+                case 'pie':
+                  Drupal.commerce_reports.charts[chart_id] = new google.visualization.PieChart(chartElement);
+                  break;
+                  
+                case 'column':
+                  Drupal.commerce_reports.charts[chart_id] = new google.visualization.ColumnChart(chartElement);
+                  break;
+                  
+              }
+              
+              if (Drupal.commerce_reports.charts[chart_id] !== undefined) {
+                Drupal.commerce_reports.charts[chart_id].draw(data, chart.options);
+              }
+            }
+            
+            google.setOnLoadCallback(drawChart);
+            
+            break;
+            
+          default:
+            // No idea
         }
+        $(value).addClass("highcharts-processed");
       }
     })
   },
--- modules/visualization/theme/commerce-reports-visualization.tpl.php
+++ modules/visualization/theme/commerce-reports-visualization.tpl.php
@@ -13,12 +13,5 @@
  *
  * @todo is this file necessary since we have a theme function?
  */
- 
-  static $js_added = false;
-  if (!$js_added) {
-    drupal_add_js(drupal_get_path('module', 'commerce_reports_visualization') . '/js/visualization.js', array('scope' => 'footer'));
-    // Flag common JS loaded status.
-    $js_added = TRUE;
-  }
 ?>
 <div id="<?php print $chart_id; ?>" class="commerce_reports-chart"></div>
